e40ab3acd994d336ad6640615a0a42db78600ece
ex <dallyshalla@gmail.com>
2016-02-10 22:32:54 +0000

    expanding bittrex next stop automation

--- trader/apps/bittrexcli.rs
+++ trader/apps/bittrexcli.rs
@@ -42,11 +42,19 @@ fn main() {
    		println!("Options available:");
    		println!("0: Exit");
    		println!("1: Check Balances");
+   		println!("2: Check Existing Orders");
+   		println!("3: Buy");
+   		println!("4: Sell");
+   		println!("5: Get Orderbook");
    		let mut input = String::new();
    		let stdin = io::stdin();
    		stdin.lock().read_line(&mut input).unwrap();
 
-    	if(input=="1\n") {
+		if(input=="0\n") {
+    		loop_index = 0;
+    		println!("goodbye");
+		}
+    	else if(input=="1\n") {
     		let the_balances = bittrexlib::get_balances(&the_api_key, &the_secret_trimmed);
     		for balance in the_balances {
     			println!("Currency {:?}", balance.Currency);
@@ -57,12 +65,111 @@ fn main() {
     		}
       		loop_index = 1;	
     	}
-		else if(input=="0\n") {
-    		loop_index = 0;
-    		println!("goodbye");
+    	else if(input=="2\n") {
+			println!("Let us know which order book");
+   			println!("Select first coin e.g. \"BTC\"");
+    		let mut firstcoin = String::new();
+    		let stdin1 = io::stdin();
+    		stdin1.lock().read_line(&mut firstcoin).unwrap();
+    		let firstcoin_trimmed = firstcoin.trim_right_matches("\n");
+   			println!("Select second coin\"MAID\"");
+    		let mut secondcoin = String::new();
+    		let stdin2 = io::stdin();
+    		stdin2.lock().read_line(&mut secondcoin).unwrap();
+    		let secondcoin_trimmed = secondcoin.trim_right_matches("\n");
+
+			let its_ts = get_openorders(&the_api_key, &the_secret_trimmed, &firstcoin_trimmed, &secondcoin_trimmed);
+			let mut count_int = 0;
+			for order in its_ts {				
+				println!("Index: {:?} Uuid: {:?} Price: {:?} Quantity: {:?} Side: {:?}", count_int, order.OrderUuid, order.Limit, order.Quantity, order.OrderType);
+				count_int += 1;			
+			}
 		}
 		else if(input=="3\n") {
+			println!("You're about to make a BUY choose two currencies, a quantity and a price.");
+   			println!("Select first coin e.g. \"BTC\"");
+    		let mut firstcoin = String::new();
+    		let stdin1 = io::stdin();
+    		stdin1.lock().read_line(&mut firstcoin).unwrap();
+    		let firstcoin_trimmed = firstcoin.trim_right_matches("\n");
+   			println!("Select second coin\"MAID\"");
+    		let mut secondcoin = String::new();
+    		let stdin2 = io::stdin();
+    		stdin2.lock().read_line(&mut secondcoin).unwrap();
+    		let secondcoin_trimmed = secondcoin.trim_right_matches("\n");
+   			println!("Select a price and quantity, a few willing sellers:");
+			let the_orderbook = get_orderbook(&the_secret_trimmed, &firstcoin_trimmed, &secondcoin_trimmed, "3");
+			println!("Quantity: {:?}  Price: {:?}", the_orderbook.sell[0].Quantity, the_orderbook.sell[0].Rate);
+			println!("Quantity: {:?}  Price: {:?}", the_orderbook.sell[1].Quantity, the_orderbook.sell[1].Rate);
+			println!("Quantity: {:?}  Price: {:?}", the_orderbook.sell[2].Quantity, the_orderbook.sell[2].Rate);
+   			println!("type in a quantity");
+    		let mut quantity = String::new();
+    		let stdin3 = io::stdin();
+    		stdin3.lock().read_line(&mut quantity).unwrap();
+    		let quantity_trimmed = quantity.trim_right_matches("\n");
+    		println!("type in a rate");
+    		let mut rate = String::new();
+    		let stdin5 = io::stdin();
+    		stdin5.lock().read_line(&mut rate).unwrap();
+    		let rate_trimmed = rate.trim_right_matches("\n");
+			let its_ts = buy_market(&the_api_key, &the_secret_trimmed, &firstcoin_trimmed, &secondcoin_trimmed, &quantity_trimmed, &rate_trimmed);
+			println!("{:?}", its_ts);
 		}
-	}
-	
+		else if(input=="4\n") {
+			println!("You're about to make a SELL choose two currencies, a quantity and a price.");
+   			println!("Select first coin e.g. \"BTC\"");
+    		let mut firstcoin = String::new();
+    		let stdin1 = io::stdin();
+    		stdin1.lock().read_line(&mut firstcoin).unwrap();
+    		let firstcoin_trimmed = firstcoin.trim_right_matches("\n");
+   			println!("Select second coin\"ETH\"");
+    		let mut secondcoin = String::new();
+    		let stdin2 = io::stdin();
+    		stdin2.lock().read_line(&mut secondcoin).unwrap();
+    		let secondcoin_trimmed = secondcoin.trim_right_matches("\n");
+   			println!("Select a price and quantity, a few willing buyers:");
+			let the_orderbook = get_orderbook(&the_secret_trimmed, &firstcoin_trimmed, &secondcoin_trimmed, "3");
+			println!("Quantity: {:?}  Price: {:?}", the_orderbook.buy[0].Quantity, the_orderbook.buy[0].Rate);
+			println!("Quantity: {:?}  Price: {:?}", the_orderbook.buy[1].Quantity, the_orderbook.buy[1].Rate);
+			println!("Quantity: {:?}  Price: {:?}", the_orderbook.buy[2].Quantity, the_orderbook.buy[2].Rate);
+   			println!("type in a quantity");
+    		let mut quantity = String::new();
+    		let stdin3 = io::stdin();
+    		stdin3.lock().read_line(&mut quantity).unwrap();
+    		let quantity_trimmed = quantity.trim_right_matches("\n");
+    		println!("type in a rate");
+    		let mut rate = String::new();
+    		let stdin5 = io::stdin();
+    		stdin5.lock().read_line(&mut rate).unwrap();
+    		let rate_trimmed = rate.trim_right_matches("\n");
+			let its_ts = sell_market(&the_api_key, &the_secret_trimmed, &firstcoin_trimmed, &secondcoin_trimmed, &quantity_trimmed, &rate_trimmed);
+			println!("{:?}", its_ts);
+		}
+		else if(input=="5\n") {
+			println!("Let us know which order book");
+   			println!("Select first coin e.g. \"BTC\"");
+    		let mut firstcoin = String::new();
+    		let stdin1 = io::stdin();
+    		stdin1.lock().read_line(&mut firstcoin).unwrap();
+    		let firstcoin_trimmed = firstcoin.trim_right_matches("\n");
+   			println!("Select second coin\"MAID\"");
+    		let mut secondcoin = String::new();
+    		let stdin2 = io::stdin();
+    		stdin2.lock().read_line(&mut secondcoin).unwrap();
+    		let secondcoin_trimmed = secondcoin.trim_right_matches("\n");
+   			println!("Select how many records you'd like to see");
+    		let mut depth = String::new();
+    		let stdin3 = io::stdin();
+    		stdin3.lock().read_line(&mut depth).unwrap();
+    		let depth_trimmed = depth.trim_right_matches("\n");
+			let its_ts = get_orderbook(&the_secret_trimmed, &firstcoin_trimmed, &secondcoin_trimmed, &depth_trimmed);
+
+			for buy in its_ts.buy {
+    			println!("BUY Quantity: {:?}                              Rate: {:?}", buy.Quantity ,buy.Rate);
+    		}
+    		for sell in its_ts.sell {
+    			println!("SELL Quantity: {:?}                              Rate: {:?}", sell.Quantity ,sell.Rate);
+    		}
+		}
+	}	
 }
--- trader/src/bittrex.rs
+++ trader/src/bittrex.rs
@@ -16,6 +16,7 @@ use std::io::Read;
 use std::thread::sleep;
 use std::time::Duration;
 use std::io::{BufRead};
+use std::ptr::null;
 
 use rustc_serialize::hex::ToHex;
 use rustc_serialize::{Decodable, Decoder};
@@ -30,15 +31,15 @@ struct Response {
 }
 
 #[derive(RustcDecodable, RustcEncodable)]
-struct OrderBookLine {
-	Quantity: f64,
-	Rate: f64,
+pub struct OrderBookLine {
+	pub Quantity: f64,
+	pub Rate: f64,
 }
 
 #[derive(RustcDecodable, RustcEncodable)]
-struct OrderBook {
-	buy: Vec<OrderBookLine>,
-	sell: Vec<OrderBookLine>,
+pub struct OrderBook {
+	pub buy: Vec<OrderBookLine>,
+	pub sell: Vec<OrderBookLine>,
 }
 
 #[derive(RustcDecodable, RustcEncodable)]
@@ -56,11 +57,41 @@ struct OurTime {
 	tv_nsec: i64,
 }
 
+#[derive(RustcDecodable, RustcEncodable)]
+pub struct TradeResponse {
+	pub success: bool,
+	pub message: String,
+	pub result: Uuid,
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+pub struct Uuid {
+	pub uuid: String,
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+pub struct OpenOrder {
+	pub Uuid: Option<String>,
+	pub OrderUuid: String,
+	pub Exchange: String,
+	pub OrderType: String,
+	pub Quantity: f64,
+	pub QuantityRemaining: f64,
+	pub Limit: f64,
+	pub CommissionPaid: f64,
+	pub Price: f64,
+	pub PricePerUnit: Option<String>,
+	pub Opened: String,
+	pub Closed: Option<String>,
+	pub CancelInitiated: bool,
+	pub ImmediateOrCancel: bool,
+	pub IsConditional: bool,
+	pub Condition: String,
+	pub ConditionTarget: Option<String>,
+}
+
 pub fn get_balances(apikey: &str, secretkey: &str) -> Vec<Balances> {
-
-
 	let api_keystring = "apikey=".to_string() + apikey;
-	println!("{:?}", &api_keystring);
 	let api_nonce = "&nonce=1";
 	let parameters = "".to_string() + &api_keystring+ &api_nonce;
 	let the_secret_bytes = secretkey.as_bytes();
@@ -89,6 +120,168 @@ pub fn get_balances(apikey: &str, secretkey: &str) -> Vec<Balances> {
     let the_balances: Vec<Balances> = json::decode(&this_data).unwrap();
     the_balances
 }
+
+
+pub fn buy_market(apikey: &str, secretkey: &str, firstcoin: &str, secondcoin: &str, quantity: &str, rate: &str) -> String {
+
+  //Buy
+//secret Key
+	let api_keystring = "apikey=".to_string() + apikey;
+	let market_selection = "&market=".to_string() + firstcoin + &"-".to_string() + secondcoin;
+	let market_quantity = "&quantity=".to_string() + quantity;
+	let market_rate = "&rate=".to_string() + rate;
+	let api_nonce = "&nonce=1";
+	let parameters = "".to_string() + &api_keystring + &market_selection + &market_quantity + &market_rate + &api_nonce;
+	let the_secret_bytes = secretkey.as_bytes();
+//nonce
+//uri
+	//let the_uri = "https://bittrex.com/api/v1.1/market/buylimit?apikey=1e69a72fa1c942ca9628001d7b628d78&market=BTC-MAID&quantity=1000&rate=0.000056&nonce=10000".to_string();
+
+	let the_begin_url = "https://bittrex.com/api/v1.1/market/buylimit?".to_string() + &parameters;
+	let the_url_clone = the_begin_url.clone();
+//hmac-sha512 signature of uri
+	let mut the_sha = Sha512::new();
+	let the_base_key = the_secret_bytes;
+	let mut the_new_mac = crypto::hmac::Hmac::new(the_sha, the_base_key);
+	the_new_mac.input(the_begin_url.as_bytes());
+	//let the_digestive = Digest::input(the_digestive, &the_uri_bytes);
+	let the_signature_string =  &the_new_mac.result().code().to_hex().to_string();
+    let resp_orderbook = http::handle()
+		.post(the_url_clone, &parameters)
+		.header("apisign", &the_signature_string)
+		.exec().unwrap();
+
+	let mut us = String::from_utf8_lossy(resp_orderbook.get_body());
+	let mut data = String::new();
+    resp_orderbook.get_body().read_to_string(&mut data).unwrap();
+
+    let json = Json::from_str(&data).unwrap();
+
+    let thetrade_string: String = json::encode(&json).unwrap();
+    thetrade_string
+
+}
+
+pub fn sell_market(apikey: &str, secretkey: &str, firstcoin: &str, secondcoin: &str, quantity: &str, rate: &str) -> String {
+
+  //Buy
+//secret Key
+	let api_keystring = "apikey=".to_string() + apikey;
+	let market_selection = "&market=".to_string() + firstcoin + &"-".to_string() + secondcoin;
+	let market_quantity = "&quantity=".to_string() + quantity;
+	let market_rate = "&rate=".to_string() + rate;
+	let api_nonce = "&nonce=1";
+	let parameters = "".to_string() + &api_keystring + &market_selection + &market_quantity + &market_rate + &api_nonce;
+	let the_secret_bytes = secretkey.as_bytes();
+//nonce
+//uri
+	//let the_uri = "https://bittrex.com/api/v1.1/market/buylimit?apikey=1e69a72fa1c942ca9628001d7b628d78&market=BTC-MAID&quantity=1000&rate=0.000056&nonce=10000".to_string();
+
+	let the_begin_url = "https://bittrex.com/api/v1.1/market/selllimit?".to_string() + &parameters;
+	let the_url_clone = the_begin_url.clone();
+//hmac-sha512 signature of uri
+	let mut the_sha = Sha512::new();
+	let the_base_key = the_secret_bytes;
+	let mut the_new_mac = crypto::hmac::Hmac::new(the_sha, the_base_key);
+	the_new_mac.input(the_begin_url.as_bytes());
+	//let the_digestive = Digest::input(the_digestive, &the_uri_bytes);
+	let the_signature_string =  &the_new_mac.result().code().to_hex().to_string();
+    let resp_orderbook = http::handle()
+		.post(the_url_clone, &parameters)
+		.header("apisign", &the_signature_string)
+		.exec().unwrap();
+
+	let mut us = String::from_utf8_lossy(resp_orderbook.get_body());
+	let mut data = String::new();
+    resp_orderbook.get_body().read_to_string(&mut data).unwrap();
+
+    let json = Json::from_str(&data).unwrap();
+
+    let thetrade_string: String = json::encode(&json).unwrap();
+    thetrade_string
+
+}
+
+
+pub fn get_openorders(apikey: &str, secretkey: &str, firstcoin: &str, secondcoin: &str) -> Vec<OpenOrder> {
+
+	let api_keystring = "apikey=".to_string() + apikey;
+	let market_selection = "&market=".to_string() + firstcoin + &"-".to_string() + secondcoin;
+	let api_nonce = "&nonce=1";
+	let parameters = "".to_string() + &api_keystring + &market_selection + &api_nonce;
+	let the_secret_bytes = secretkey.as_bytes();
+//nonce
+
+	let the_begin_url = "https://bittrex.com/api/v1.1/market/getopenorders?".to_string() + &parameters;
+	let the_url_clone = the_begin_url.clone();
+//uri
+//hmac-sha512 signature of uri
+	let mut the_sha = Sha512::new();
+	let the_base_key = the_secret_bytes;
+	let mut the_new_mac = crypto::hmac::Hmac::new(the_sha, the_base_key);
+	the_new_mac.input(the_begin_url.as_bytes());
+	//let the_digestive = Digest::input(the_digestive, &the_uri_bytes);
+	let the_signature_string =  &the_new_mac.result().code().to_hex().to_string();
+    let resp_openorders = http::handle()
+		.post(the_url_clone, &parameters)
+		.header("apisign", &the_signature_string)
+		.exec().unwrap();
+
+	let mut us = String::from_utf8_lossy(resp_openorders.get_body());
+	let mut data = String::new();
+    resp_openorders.get_body().read_to_string(&mut data).unwrap();
+
+    let orderbook_json = Json::from_str(&data).unwrap();
+    let orderbook_result = orderbook_json.find("result");
+    let orderbook_result_string: String = json::encode(&orderbook_result).unwrap();
+   
+
+    let orderbook: Vec<OpenOrder> = json::decode(&orderbook_result_string).unwrap();
+
+    orderbook
+}
+
+
+pub fn get_orderbook(secretkey: &str, firstcoin: &str, secondcoin: &str, depth: &str) -> OrderBook {
+
+	let market_selection = "market=".to_string() + firstcoin + &"-".to_string() + secondcoin;
+	let market_type = "&type=both".to_string();
+	let market_depth = "&depth=".to_string() + depth;
+	let parameters = "".to_string() + &market_selection + &market_type + &market_depth;
+	let the_secret_bytes = secretkey.as_bytes();
+//nonce
+
+	let the_begin_url = "https://bittrex.com/api/v1.1/public/getorderbook?".to_string() + &parameters;
+	let the_url_clone = the_begin_url.clone();
+//uri
+//hmac-sha512 signature of uri
+	let mut the_sha = Sha512::new();
+	let the_base_key = the_secret_bytes;
+	let mut the_new_mac = crypto::hmac::Hmac::new(the_sha, the_base_key);
+	the_new_mac.input(the_begin_url.as_bytes());
+	//let the_digestive = Digest::input(the_digestive, &the_uri_bytes);
+	let the_signature_string =  &the_new_mac.result().code().to_hex().to_string();
+    let resp_openorders = http::handle()
+		.post(the_url_clone, &parameters)
+		.header("apisign", &the_signature_string)
+		.exec().unwrap();
+
+	let mut us = String::from_utf8_lossy(resp_openorders.get_body());
+	let mut data = String::new();
+    resp_openorders.get_body().read_to_string(&mut data).unwrap();
+
+    let orderbook_json = Json::from_str(&data).unwrap();
+    let orderbook_result = orderbook_json.find("result");
+    let orderbook_result_string: String = json::encode(&orderbook_result).unwrap();
+
+    let orderbook: OrderBook = json::decode(&orderbook_result_string).unwrap();
+    orderbook
+}
+
+
+
+
+
 
 #[test]
 fn it_works() {
